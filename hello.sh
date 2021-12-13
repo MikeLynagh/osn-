71e1898684b0cb6d81e4c6583ed3857aa7735e4c
MikeLynagh <80194931+MikeLynagh@users.noreply.github.com>
2021-12-13 07:32:57 +0000

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-echo "Hello World"
+####
+#Accept a file as an argument
+####
 
-echo "Hi"
+filename=$1
+
+####
+#reads through that file one line at a time
+####
+
+
+while read line; do
+#reading each line
+echo $line
+done < $filename
+
+for FILE in *; do 
+echo -e "$FILE\nLoops Rule\!" > $FILE; done
