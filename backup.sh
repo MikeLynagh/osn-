73660d241da57e75b82e41aa8d674ea3e4eed565
MikeLynagh <80194931+MikeLynagh@users.noreply.github.com>
2021-12-13 18:11:11 +0000

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -6,17 +6,18 @@
 
 filename=$1
 
+for FILE in *; do echo -e "$FILE\nLoops Rule\!" > $FILE; cat $FILE; done
 ####
 #reads through that file one line at a time
 ####
 
 
-while read line; do
+#while read line; do
 #reading each line
-echo $line
-done < $filename
+#echo $line
+#done < $filename
 
 
-for FILE in filename; do
-echo $FILE;
-done
+#for FILE in filename; do
+#echo $FILE;
+#done
