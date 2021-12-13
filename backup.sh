64dde0c1101fb0a3d8715c573793ab2038caf402
MikeLynagh <80194931+MikeLynagh@users.noreply.github.com>
2021-12-13 07:12:12 +0000

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -11,38 +11,11 @@ filename=$1
 ####
 
 
-#while read line; do
+while read line; do
 #reading each line
-#echo $line
-#done < $filename
+echo $line
+done < $filename
 
 ####
 # attempt to cycle through each file and back it up
 ####
-
-while read line; do
-#echo $line
-timestamp=date +%b-%d-%y
-sudo tar -cvpzf /home/backup-$timestamp.tar.gz filename
-done <$filename
-
-####
-# Check if each home directory contains a file name.backup
-####
-
-test -f/tmp/backup
-echo $?
-
-####
-# if a file called tmp/backup does not exist, make  a file with that name
-###
-
-
-if [[ ! -e/tmp/backup ]]; then
-touch /tmp/backup
-echo "a file called backup was created"
-else
-echo "a file called backup already exists"
-fi
-done
-
