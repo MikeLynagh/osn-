177c5412e1029caee753f0b4c5995e3c1facf113
MikeLynagh <80194931+MikeLynagh@users.noreply.github.com>
2021-12-13 18:16:00 +0000

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -6,7 +6,14 @@
 
 filename=$1
 
-for FILE in *; do echo -e "$FILE\nLoops Rule\!" > $FILE; cat $FILE; done
+while read line; do 
+cp $filename "$filename.bak"; 
+done;
+
+## this will loop through directory and update content wtih 
+##filename and the phrase loops rule 
+
+#for FILE in *; do echo -e "$FILE\nLoops Rule\!" > $FILE; cat $FILE; done
 ####
 #reads through that file one line at a time
 ####
