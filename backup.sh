817540be535c24b76f9cb705de9ef351e8b297d5
MikeLynagh <80194931+MikeLynagh@users.noreply.github.com>
2021-12-16 06:41:09 +0000

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -7,11 +7,9 @@ echo "Starting backup task "
 ####
 
 
-fILENAME=$1
+filename=$1
 
-FILE1=/home/${USER}/*.*
-FILE2=/tmp/backup/${USER}/*.*
-a=1
+
 
 ####
 # Go through that file and print all of the users in that file 
@@ -30,7 +28,8 @@ while read -r line; do
 echo "beginning to run backup file check on files"
 if [ ! -f /home/"${line}"/.backup ];
 echo "there is no backup file for ${line}"
-then  touch /home/${fILENAME}/.backup
+then  touch /home/${line}/.backup
+echo "created /home/$line/.backup"
 fi
 
 done < $filename
@@ -50,16 +49,40 @@ fi
 ####
 
 
+# loop through all the files in the source called $FILE1
+# list everything in recursive path
 
-while
-if [[ -d "/home/${USER}" ]]
-then
-   while read -r line $file1; do
-   while read -r line $file2;do
-       if [ $file1==$file2 ] ; then
-for i in $file1; do
-new=$(printf "%file2." "$file2$a.")
-mv "$i" "$new"
-let a=a++
+FILE1=/home/${USER}
+FILE2=/tmp/backup/${USER}
+
+
+for file in $(find $FILE1 -printf "%P\n") ; do
+
+# check if the file in FILE2 has already appeared in $FILE1
+    if [ -a $FILE2/$file ] ; then
+# if the file in FILE1 is newer than  FILE2
+        if [ $FILE1/$file -nt $FILE2/$file ] ; then
+        echo "newer file detected, copying"
+# copy files from FILE1  to FILE2
+        cp -r $FILE1/$file $FILE2/$file
+        else
+        echo "file $file exists, skipping"
+        fi
+    else
+    echo $file " is being copied over to $FILE2"
+    cp -r $FILE1/$file $FILE2/$file
+    fi
 done
-fi
+
+# while
+# if [[ -d "/home/${USER}" ]]
+# then
+#    while read -r line $file1; do
+#    while read -r line $file2;do
+#        if [ $file1==$file2 ] ; then
+# for i in $file1; do
+# new=$(printf "%file2." "$file2$a.")
+# mv "$i" "$new"
+# let a=a++
+# done
+# fi;
